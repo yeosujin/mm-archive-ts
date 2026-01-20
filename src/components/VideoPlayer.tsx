42767265fb6fe7425d2aaa2edc571ff6b9b3b008
여수진 <suujin@mz.co.kr>
2026-01-20 14:06:54 +0900

    fix: adjust video thumbnail fragment to #t=0.1 and set preload=auto

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -13,7 +13,7 @@ const VideoPlayer = memo(({ videoUrl, className = '' }: Props) => {
         playsInline
         webkit-playsinline="true"
         controlsList="nodownload"
-        preload="metadata"
+        preload="auto"
         style={{
           width: '100%',
           maxWidth: '800px',
@@ -21,9 +21,9 @@ const VideoPlayer = memo(({ videoUrl, className = '' }: Props) => {
           backgroundColor: '#000',
         }}
       >
-        <source src={`${videoUrl}#t=0.001`} type="video/mp4" />
-        <source src={`${videoUrl}#t=0.001`} type="video/webm" />
-        <source src={`${videoUrl}#t=0.001`} type="video/ogg" />
+        <source src={`${videoUrl}#t=0.1`} type="video/mp4" />
+        <source src={`${videoUrl}#t=0.1`} type="video/webm" />
+        <source src={`${videoUrl}#t=0.1`} type="video/ogg" />
         브라우저가 비디오 재생을 지원하지 않습니다.
       </video>
     </div>
